965af25ac397a90338effd4276f52a4b65dfda86
cbrock <colintbrock@gmail.com>
2015-01-17 15:19:23 -0800

    Only target play buttons which are currently playing

--- controls.js
+++ controls.js
@@ -15,7 +15,7 @@ chrome.browserAction.onClicked.addListener( function () {
   chrome.tabs.query(options, function (tabs) {
 
     var details = {
-      code: 'document.getElementsByClassName("play_pause")[0].click();'
+      code: 'document.getElementsByClassName("play_pause playing")[0].click();'
     };
     tabs.forEach(function (tab) {
       chrome.tabs.executeScript(tab.id, details);
